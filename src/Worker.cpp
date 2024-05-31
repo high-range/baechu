7c6f9e7b03a95d9fbab54faede7202131c0ff34b
leesiha <mlice1030@soongsil.ac.kr>
2024-05-31 23:26:51 +0900

    Worker class refined

--- src/Worker.cpp
+++ src/Worker.cpp
@@ -1,66 +1,131 @@
 #include "Worker.hpp"
 
+#include <dirent.h>
 #include <sys/stat.h>
 
-#include <cstdio>
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "Configuration.hpp"
 #include "Request.hpp"
 #include "Response.hpp"
 
-std::string Worker::getPath(Request request) {
-    // Get path
-    std::string rootDirectory =
-        Configuration::getRootDirectory(request.getHost());
-    return (rootDirectory + request.getUrl());
+std::string Worker::getFullPath(const std::string& host,
+                                const std::string& path) {
+    Configuration config(host);
+    std::string rootDirectory = config.getRootDirectory();
+    return rootDirectory + path;
 }
 
-bool Worker::isStaticRequest(Request request) {
-    // Check if request is static
-    return true;
+bool Worker::isStaticRequest(const Request& request) {
+    const std::string& method = request.getMethod();
+    return (method == "GET" || method == "POST" || method == "DELETE");
 }
 
-Response Worker::handelStaticRequest(Request request) {
-    // Handle static request
-    std::string method = request.getMethod();
-    if (method == "GET") {
-        doGet(request);
-    } else if (method == "POST") {
-        doPost(request);
-    } else if (method == "DELETE") {
-        doDelete(request);
-    } else {
-        // Return 405 Method Not Allowed for unsupported methods
-        // Response(405, "Method Not Allowed");
+Response Worker::handleStaticRequest(const Request& request) {
+    const std::string& method = request.getMethod();
+    std::string body;
+
+    try {
+        if (method == "GET") {
+            body = doGet(request);
+        } else if (method == "POST") {
+            body = doPost(request);
+        } else if (method == "DELETE") {
+            body = doDelete(request);
+        } else {
+            return Response(405, "Method Not Allowed");
+        }
+        return Response(200, body);
+    } catch (const char* err) {
+        return Response(500, err);
     }
-    return Response();
 }
 
-void Worker::doGet(Request request) {
-    // Handle GET request
-    std::string path = getPath(request);
+Response Worker::handleDynamicRequest(const Request& request) {
+    return Response(501, "Not Implemented");
 }
 
-std::string generate_unique_filename() {
-    // Get the current time since epoch
+bool isFile(const std::string& fullPath) {
+    struct stat statBuffer;
+    if (stat(fullPath.c_str(), &statBuffer) != 0) {
+        throw "stat failed";
+    }
+
+    return S_ISREG(statBuffer.st_mode);
+}
+
+std::string doGetFile(const std::string& fullPath) {
+    std::ifstream file(fullPath);
+    if (!file.is_open()) {
+        throw "file not found";
+    }
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    return buffer.str();
+}
+
+std::string getFileLink(const std::string& hostname, int port,
+                        const std::string& dirname,
+                        const std::string& filename) {
+    std::ostringstream ss;
+    ss << "<p><a href=\"http://" << hostname << ":" << port << dirname
+       << filename << "\">" << filename << "</a></p>\n";
+    return ss.str();
+}
+
+std::string generateHTML(const std::string& fullPath, const std::string& host,
+                         int port, const std::string& path) {
+    DIR* dir = opendir(fullPath.c_str());
+    if (dir == nullptr) {
+        throw "could not open directory";
+    }
+
+    std::ostringstream ss;
+    ss << "<!DOCTYPE html>\n<html>\n<head>\n<title>Index of " << path
+       << "</title>\n</head>\n<body>\n<h1>Index of " << path << "</h1>\n";
+
+    for (struct dirent* dirEntry = readdir(dir); dirEntry;
+         dirEntry = readdir(dir)) {
+        ss << getFileLink(host, port, path, dirEntry->d_name);
+    }
+
+    ss << "</body>\n</html>\n";
+    closedir(dir);
+
+    return ss.str();
+}
+
+std::string doGetDirectory(const std::string& fullPath, const std::string& host,
+                           const std::string& path) {
+    return generateHTML(fullPath, host, 8080, path);
+}
+
+std::string Worker::doGet(const Request& request) {
+    std::string fullPath = getFullPath(request.getHost(), request.getUrl());
+    if (isFile(fullPath)) {
+        return doGetFile(fullPath);
+    }
+    return doGetDirectory(fullPath, request.getHost(), request.getUrl());
+}
+
+std::string generateFilename() {
     std::time_t now = std::time(0);
-
-    std::srand(std::time(0));
-    int random_number =
-        std::rand() % 9000 + 1;  // Generate a random number between 1 and 9000
-
-    // Combine time and random number to create a unique filename
-    std::string filename = "saved_file_" + std::to_string(now) + "_" +
-                           std::to_string(random_number) + ".html";
-
-    return filename;
+    int random_number = std::rand() % 9000 + 1;
+    std::ostringstream filename;
+    filename << "saved_file_" << now << "_" << random_number << ".html";
+    // return filename.str();
+    return "saved_file.html";
 }
 
-bool Worker::saveFile(const std::string& dir, const std::string& content) {
-    std::string filename = generate_unique_filename();
+bool saveFile(const std::string& dir, const std::string& content) {
+    std::string filename = generateFilename();
     std::string path = dir + "/" + filename;
     std::ofstream file(path);
     if (file.is_open()) {
@@ -73,36 +138,35 @@ bool Worker::saveFile(const std::string& dir, const std::string& content) {
     }
 }
 
-void Worker::doPost(Request request) {
-    std::string dir = getPath(request);
+std::string Worker::doPost(const Request& request) {
+    std::string fullPath = getFullPath(request.getHost(), request.getUrl());
     std::string content = request.getBody();
 
-    if (saveFile(dir, content)) {
-        return;  // Return 201 Created if successful
+    if (saveFile(fullPath, content)) {
+        return content;  // Return 201 Created if successful
     } else {
-        return;  // Return 500 Internal Server Error
+        return "";  // Return 500 Internal Server Error
     }
 }
 
-void Worker::doDelete(Request request) {
-    std::string path = getPath(request);
+std::string Worker::doDelete(const Request& request) {
+    std::string fullPath = getFullPath(request.getHost(), request.getUrl());
     struct stat buffer;
-    if (stat(path.c_str(), &buffer) == 0) {
-        if (std::remove(path.c_str()) == 0) {
-            return;  // Return 200 OK if successful
+    if (stat(fullPath.c_str(), &buffer) == 0) {
+        if (std::remove(fullPath.c_str()) == 0) {
+            return "";  // Return 204 No Content if successful
+        } else {
+            throw "DELETE failed";
         }
     } else {
-        return;  // Return 404 Not Found if the file does not exist
+        return "";  // Return 404 Not Found if the file does not exist
     }
 }
 
-Response Worker::handleRequest(Request request) {
-    // Handle request
-    std::string path = getPath(request);
-
+Response Worker::handleRequest(const Request& request) {
     if (isStaticRequest(request)) {
-        return handelStaticRequest(request);
+        return handleStaticRequest(request);
     } else {
-        // return handleDynamicRequest(request);
+        return handleDynamicRequest(request);
     }
 }
--- src/Worker.hpp
+++ src/Worker.hpp
@@ -1,24 +1,34 @@
 #pragma once
 
-#include <iostream>
+#include <string>
 
 #include "Request.hpp"
 #include "Response.hpp"
 
 class Worker {
   public:
-    Response handleRequest(Request request);
+    Response handleRequest(const Request& request);
 
   private:
-    std::string getPath(Request request);
-    bool isStaticRequest(Request request);
-    Response handelStaticRequest(Request request);
-    Response handleDynamicRequest(Request request);
+    std::string getFullPath(const std::string& host, const std::string& path);
+    bool isStaticRequest(const Request& request);
+    Response handleStaticRequest(const Request& request);
+    Response handleDynamicRequest(const Request& request);
 
-    void doGet(Request request);
-
-    void doPost(Request request);
-    bool saveFile(const std::string& path, const std::string& content);
-
-    void doDelete(Request request);
+    std::string doGet(const Request& request);
+    std::string doPost(const Request& request);
+    std::string doDelete(const Request& request);
 };
+
+// Utility functions used in Worker.cpp
+bool isFile(const std::string& fullPath);
+std::string doGetFile(const std::string& fullPath);
+std::string getFileLink(const std::string& hostname, int port,
+                        const std::string& dirname,
+                        const std::string& filename);
+std::string generateHTML(const std::string& fullPath, const std::string& host,
+                         int port, const std::string& path);
+std::string doGetDirectory(const std::string& fullPath, const std::string& host,
+                           const std::string& path);
+std::string generateFilename();
+bool saveFile(const std::string& dir, const std::string& content);
