c8817579329e7aed6c775fc229a9ee2f236c30cf
Taehyeon Kim <taehyeonkim@Taehyeonui-MacBookAir.local>
2024-06-01 16:31:46 +0900

    manager, request, response, requestData 객체 헤더 생성

--- src/Request.hpp
+++ src/Request.hpp
@@ -5,29 +5,16 @@
 
 class RequestData;
 class Configuration;
+#include <iostream>
+#include <vector>
+
+class RequestData;
+class Configuration;
 
 class Request {
   public:
     virtual ~Request() = 0;
-    static void MessageParse(std::string& requestMessage,
+    static void messageParse(std::string& requestMessage,
                              RequestData& requestData,
                              const Configuration& configuration);
-
-  private:
-    enum State {
-        Method,
-        Path,
-        Version,
-        FieldName,
-        OWS,
-        RWS,
-        FirstFieldContent,
-        SecondFieldContent,
-        ObsFold,
-        End
-    } state;
-    bool Request::isTChar(const char c);
-    bool Request::isVChar(const char c);
-    bool Request::isWS(const char c);
-    bool Request::isCRLF(const char now, const char next);
 };
