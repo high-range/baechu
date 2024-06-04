4eb9de7f0533eed7026f7f2ae08424140983c0ad
Taehyeon Kim <taehyeonkim@Taehyeonui-MacBookAir.local>
2024-06-04 22:10:36 +0900

    request message startline/header parsing complete

--- src/Request.hpp
+++ src/Request.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cstring>
 #include <iostream>
+#include <string>
 #include <vector>
 
 class RequestData;
@@ -12,4 +14,36 @@ class Request {
     static void messageParse(std::string& requestMessage,
                              RequestData& requestData,
                              const Configuration& configuration);
+
+    enum State {
+        StartLineStart,
+        Method,
+        MethodEnd,
+        RequestTargetStart,
+        AbsolutePath,
+        Query,
+        RequestTargetEnd,
+        HTTPVersion,
+        StartLineEnd,
+        HeaderStart,
+        FieldName,
+        WhiteSpace,
+        FieldValue,
+        FieldContent,
+        ObsFold,
+        HeaderEnd,
+        Body
+    };
+
+    static bool isObsFold(const char* str);
+    static bool isWS(const char c);
+    static bool isFieldVchar(const char c);
+    static bool isPchar(const char* str);
+    static bool isHexDigit(const char c);
+    static bool isHttpVersion(const char* str);
+    static bool isTchar(const char c);
+    static bool isCRLF(const char* str);
+    static std::string th_substr(const char* src, const int start,
+                                 const int end);
+    static std::string th_strtrim(const std::string& src, const char target);
 };
