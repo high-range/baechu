df39e4539b411c73e0a801d5ed65ecd388a38a52
enihsgnir <enihsgnir@gmail.com>
2024-06-20 18:03:05 +0900

    handle header key case-consistency

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,5 +1,18 @@
 #include "Response.hpp"
 
+static std::string camel(std::string s) {
+    bool capitalize = true;
+    for (size_t i = 0; i < s.length(); i++) {
+        if (s[i] == '-') {
+            capitalize = true;
+        } else if (capitalize) {
+            s[i] = toupper(s[i]);
+            capitalize = false;
+        }
+    }
+    return s;
+}
+
 std::string Response::messageGenerate(ResponseData data) {
     std::ostringstream ss;
 
@@ -8,7 +21,7 @@ std::string Response::messageGenerate(ResponseData data) {
 
     Headers headers = data.headers;
     for (Headers::iterator it = headers.begin(); it != headers.end(); it++) {
-        ss << it->first << ": " << it->second << "\r\n";
+        ss << camel(it->first) << ": " << it->second << "\r\n";
     }
 
     ss << "\r\n";
