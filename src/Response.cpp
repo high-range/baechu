50a56996e3139cfc4a40de2e5649f5da78820825
enihsgnir <enihsgnir@gmail.com>
2024-06-28 17:14:03 +0900

    refactor reason phrases to function

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,5 +1,21 @@
 #include "Response.hpp"
 
+static std::string getReasonPhrase(int statusCode) {
+    static std::map<int, std::string> reasonPhrases;
+    if (reasonPhrases.empty()) {
+        reasonPhrases[200] = "OK";
+        reasonPhrases[201] = "Created";
+        reasonPhrases[204] = "No Content";
+        reasonPhrases[400] = "Bad Request";
+        reasonPhrases[404] = "Not Found";
+        reasonPhrases[405] = "Method Not Allowed";
+        reasonPhrases[500] = "Internal Server Error";
+        reasonPhrases[501] = "Not Implemented";
+    }
+
+    return reasonPhrases[statusCode];
+}
+
 static std::string camel(std::string s) {
     bool capitalize = true;
     for (size_t i = 0; i < s.length(); i++) {
@@ -17,7 +33,7 @@ std::string Response::messageGenerate(ResponseData data) {
     std::ostringstream ss;
 
     ss << "HTTP/1.1 " << data.statusCode << " "
-       << reasonPhrases[data.statusCode] << "\r\n";
+       << getReasonPhrase(data.statusCode) << "\r\n";
 
     Headers headers = data.headers;
     headers["content-length"] = std::to_string(data.body.length());
