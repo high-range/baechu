bd5950e6714425466f5718daf5697353145243b7
SuperStarKang <rkdalsfpdl@gmail.com>
2024-06-29 19:05:54 +0900

    check syntax error && refactoring getRootDirectory use port_number, location, server_name

--- test/ConfigTest.cpp
+++ test/ConfigTest.cpp
@@ -9,36 +9,18 @@ int main(int argc, char** argv) {
     Configuration& config = Configuration::getInstance();
     config.initialize(filename);
 
-    std::string host = "";
-    std::string port = "8080";
-    std::string location = "";
-    std::cout << "3parameters root: "
-              << config.getRootDirectory(host, port, location) << std::endl;
-
-    std::string path = "localhost:8080/";
-    std::string root = config.getRootDirectory(path);
-    std::cout << "fullPath root: " << root << std::endl;
-
-    std::cout << "default port: " << config.getDefaultPort() << std::endl;
-
-    std::cout << "client max body size: " << config.getClientMaxBodySize(path)
+    std::string port = "80";
+    std::string serever_name = "localhost";
+    std::string location = "/a/c";
+    std::cout << "root: "
+              << config.getRootDirectory(port, location, serever_name)
               << std::endl;
 
-    std::vector<std::string> cgi_extensions = config.getCgiExtensions(path);
-    std::cout << "cgi: ";
-    for (size_t i = 0; i < cgi_extensions.size(); i++) {
-        std::cout << cgi_extensions[i] << " ";
+    std::vector<std::string> ports = config.getPortNumbers();
+    std::cout << "ports: ";
+    for (size_t i = 0; i < ports.size(); i++) {
+        std::cout << ports[i] << ' ';
     }
     std::cout << std::endl;
-
-    std::cout << "methods: " << config.getAllowedMethods(path) << ' '
-              << config.isMethodAllowedFor(path, "GET") << std::endl;
-
-    std::cout << "autoindex: " << config.isDirectoryListingEnabled(path)
-              << std::endl;
-
-    std::cout << "error_page: " << config.getErrorPageFromServer(path)
-              << std::endl;
-
     return 0;
 }
