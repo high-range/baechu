28dfe204995f9fc7248a181ecdc7a03e408183c2
enihsgnir <enihsgnir@gmail.com>
2024-06-30 11:19:46 +0900

    rename to server name

--- src/Worker.hpp
+++ src/Worker.hpp
@@ -29,10 +29,9 @@ class Worker {
 
     std::map<std::string, std::string> header;  // Response header
 
-    std::string host;
-    std::string domain;
     std::string ip;
     std::string port;
+    std::string serverName;
 
     std::string path;
     std::string location;
