888483a9ecdc9260fb85d0bf39aeec5df59589e4
leesiha <mlice1030@soongsil.ac.kr>
2024-07-05 17:31:26 +0900

    Update website

--- test/defence/script.js
+++ test/defence/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-
     document.getElementById('scroll-left').addEventListener('click', function () {
         document.querySelector('.releases').scrollBy({
             left: -300, // Adjust this value based on the width of .release-item
@@ -14,3 +13,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
+
+// Function to load content into the container
+function loadContent(page, buttonElement) {
+    const contentContainer = document.getElementById('content-container');
+    contentContainer.style.display = 'block';
+    
+    // Fade out current content
+    contentContainer.style.opacity = 0;
+    contentContainer.style.transition = 'opacity 0.2s';
+    
+    // Wait for the fade-out to complete
+    setTimeout(() => {
+        fetch(page)
+            .then(response => response.text())
+            .then(data => {
+                const releaseItem = buttonElement.closest('.release-item');
+                const bgColor = window.getComputedStyle(releaseItem).backgroundColor;
+                contentContainer.innerHTML = data;
+                contentContainer.style.backgroundColor = bgColor;
+                
+                // Fade in new content
+                contentContainer.style.opacity = 1;
+            })
+            .catch(error => console.error('Error loading content:', error));
+    }, 500); // Match this delay with the fade-out duration
+}
