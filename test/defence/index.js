bd56e5ff350b4e3aea6b4bc1bd2641d00e9c3f63
leesiha <mlice1030@soongsil.ac.kr>
2024-07-06 05:16:19 +0900

    chore: Remove commented out release items in index.html and update methods.js

--- test/defence/index.js
+++ test/defence/index.js
@@ -18,11 +18,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 function loadContent(page, buttonElement) {
     const contentContainer = document.getElementById('content-container');
     contentContainer.style.display = 'block';
-    
+
     // Fade out current content
     contentContainer.style.opacity = 0;
     contentContainer.style.transition = 'opacity 0.2s';
-    
+
     // Wait for the fade-out to complete
     setTimeout(() => {
         fetch(page)
@@ -32,14 +32,14 @@ function loadContent(page, buttonElement) {
                 const bgColor = window.getComputedStyle(releaseItem).backgroundColor;
                 contentContainer.innerHTML = data;
                 contentContainer.style.backgroundColor = bgColor;
-                
+
                 // Fade in new content
                 contentContainer.style.opacity = 1;
 
                 // Dynamically load JavaScript file for the loaded content
-                if (page === 'evaluation.html') {
+                if (page === 'methods.html') {
                     const script = document.createElement('script');
-                    script.src = 'evaluation.js';
+                    script.src = 'methods.js';
                     document.body.appendChild(script);
                 }
             })
